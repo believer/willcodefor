0cc9f9d3d42e42a04fd5d6e03963a6e4a11718d1
Rickard Natt och Dag <rickard@nattochdag.dev>
2023-08-28 12:58:23 +0200

    fix(hasher): use shasum
    shasum seems to be more general than sha256sum

--- hasher.sh
+++ hasher.sh
@@ -12,7 +12,7 @@ npx tailwindcss -i ./styles.css -o ./public/styles.css
 # of the generated CSS file. This is to bust the cache of the CSS file.
 
 # Get the hash of the generated CSS file
-hash=$(sha256sum ./public/styles.css | cut -c1-6)
+hash=$(shasum -a 256 ./public/styles.css | cut -c1-6)
 
 # Rename the generated CSS file
 mv ./public/styles.css ./public/styles.$hash.css
@@ -27,11 +27,11 @@ echo "Tailwind generated and updated"
 
 # Fixing HTMX scripts
 
-hash=$(sha256sum ./public/htmx.*.min.js | cut -c1-6)
+hash=$(shasum -a 256 ./public/htmx.*.min.js | cut -c1-6)
 mv ./public/htmx.*.min.js ./public/htmx.$hash.min.js
 sed -i "" "s/htmx\.[a-z0-9]\{6\}\.min/htmx\.$hash\.min/g" ./views/layouts/main.html
 
-hash=$(sha256sum ./public/htmx-head-support.*.js | cut -c1-6)
+hash=$(shasum -a 256 ./public/htmx-head-support.*.js | cut -c1-6)
 mv ./public/htmx-head-support.*.js ./public/htmx-head-support.$hash.js
 sed -i "" "s/htmx-head-support\.[a-z0-9]\{6\}/htmx-head-support\.$hash/g" ./views/layouts/main.html
 
@@ -39,7 +39,7 @@ echo "HTMX scripts fixed"
 
 # Fixing Command menu script
 
-hash=$(sha256sum ./public/command-menu.*.js | cut -c1-6)
+hash=$(shasum -a 256 ./public/command-menu.*.js | cut -c1-6)
 mv ./public/command-menu.*.js ./public/command-menu.$hash.js
 sed -i "" "s/command-menu\.[a-z0-9]\{6\}/command-menu\.$hash/g" ./views/layouts/main.html
 
