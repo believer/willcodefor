af184e3704a16181201eb9fe150f8454305acf78
Rickard Natt och Dag <rickard@nattochdag.dev>
2023-09-01 09:22:03 +0200

    chore: update dependencies

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/believer/willcodefor-go
 
-go 1.20
+go 1.21
+
+toolchain go1.21.0
 
 require (
 	github.com/alecthomas/chroma/v2 v2.8.0
--- utils/markdown.go
+++ utils/markdown.go
@@ -58,6 +58,7 @@ func MarkdownToHTML(input []byte) bytes.Buffer {
 				Texter: anchor.Text("#"),
 			},
 			extension.Strikethrough,
+			extension.Typographer,
 			extension.NewFootnote(
 				extension.WithFootnoteBacklinkClass([]byte("font-mono no-underline")),
 			),
